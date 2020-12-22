8e8da7df18cc1990497f7709e052cc0c3f76bb9e
J-O-B <Jonathanmichaelobrien@gmail.com>
2020-12-22 15:15:42 +0000

    added basic about page with some text and carousel, no styling

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,11 @@
 // Materialize Required JavaScript
 $(document).ready(function(){
+    // Side Navbar
     $('.sidenav').sidenav();
+    // Time Picker In Forms
     $('.timepicker').timepicker();
+    // Select Function In Forms
     $('select').formSelect();
+    // Carousel Functionality
+    $('.carousel').carousel();
   });
