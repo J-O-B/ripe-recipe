4ddebf0b8d63b305edef82d334f8d7af752ea3dd
J-O-B <Jonathanmichaelobrien@gmail.com>
2021-01-07 01:09:15 +0000

    more basic layout for homepage, javascript changed to reflect new layout and css also added

--- static/js/script.js
+++ static/js/script.js
@@ -13,12 +13,7 @@ $(document).ready(function(){
     $('.carousel').carousel();
 
     // Homepage States
-    $('.welcome-card').fadeIn(3000);
     $('.about-ripe-expanded').hide();
-    $('#firstCard').hide();
-    $('#secondCard').hide();
-    $('#thirdCard').hide();
-
   });
 
   // Full Width Slider 
@@ -39,49 +34,6 @@ dateGen();
 
 $('#about-ripe').click(function(){
     // Initial View
-    $('.welcome-card').fadeOut(1000);
     $('.about-ripe-expanded').fadeIn(1000);
-    // track where user is using variable:
-    var x = 1;
-    cards(1);
+    
 });
-
-function cards(x){
-    $('#firstCard').hide();
-    $('#secondCard').hide();
-    $('#thirdCard').hide();
-
-    if (x == 1){
-        $('#firstCard').show();
-        $('.next').click(function(){
-            x=2;
-            cards(x);
-        });
-        $('.previous').click(function(){
-            x = 3;
-            cards(x);
-        });
-    }
-    else if (x == 2){
-        $('#secondCard').show();
-        $('.next').click(function(){
-            x=3;
-            cards(x);
-        });
-        $('.previous').click(function(){
-            x = 1;
-            cards(x);
-        });
-    }
-    else if (x == 3){
-        $('#thirdCard').show();
-        $('.next').click(function(){
-            x=1;
-            cards(x);
-        });
-        $('.previous').click(function(){
-            x = 2;
-            cards(x);
-        });
-    }
-}
